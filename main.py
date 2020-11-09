0a58e45fe7f1cef8a2a3f17e2564856637675bc8
Ryan Ereno <ryanaereno@gmail.com>
2020-11-08 22:55:32 -0800

    New functions added

--- main.py
+++ main.py
@@ -5,39 +5,45 @@ from bs4 import BeautifulSoup as soup
 EMAIL_ADDRESS = 'NotifyingPrice@gmail.com'
 EMAIL_PASSWORD = 'FuckChegg123'
 RECIPIENT = ''
-MSG = ''
+MSG = 'The price for your item has reached your desired price!'
+
+def main():
+    #telling amazon that im a real browser
+    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+
+    my_url = 'http://rb.gy/kjkteb'
+
+    resp = requests.get(my_url, headers = headers)
+
+    ps = soup(resp.content, 'lxml')
+
+    container = ps.findAll("span", {"id": "price_inside_buybox"})
+    temp_price = container[0].text
+
+    actual_price = float(temp_price[2:len(temp_price)])
 
 
-#server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
+    my_price = float(input())
 
-#server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-#server.sendmail(EMAIL_ADDRESS,
-#                RECIPIENT,
-#                MSG)
+    checkPrice(my_price, actual_price)
 
-#server.quit()
+def sendEmail():
+    server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
+    server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    server.sendmail(EMAIL_ADDRESS,
+                    RECIPIENT,
+                    MSG)
 
-headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    server.quit()
 
-my_url = 'http://rb.gy/kjkteb'
+def checkPrice(my_price, actual_price):
+    if my_price <= actual_price:
 
-resp = requests.get(my_url, headers = headers)
+        sendEmail()
 
-
-ps = soup(resp.content)
-
-container = ps.findAll("span", {"id": "price_inside_buybox"})
-
-temp = container[0].text
-
-actual = float(temp[2:len(temp)])
+    else:
+        print("Nah G")
 
 
-requested = float(input())
-
-if requested == actual:
-    print("Yurt")
-
-else:
-    print("Nah G")
+main()
