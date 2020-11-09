64d4a0515cb558e23a7168db6e3399853cb8d73d
Ryan Ereno <ryanaereno@gmail.com>
2020-11-08 22:39:07 -0800

    added libraries and removed lxml

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ RECIPIENT = ''
 MSG = ''
 
 
-server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
-server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-server.sendmail(EMAIL_ADDRESS,
-                RECIPIENT,
-                MSG)
+#server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
 
-server.quit()
+#server.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+#server.sendmail(EMAIL_ADDRESS,
+#                RECIPIENT,
+#                MSG)
+
+#server.quit()
 
 
 headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
@@ -24,7 +25,7 @@ my_url = 'http://rb.gy/kjkteb'
 resp = requests.get(my_url, headers = headers)
 
 
-ps = soup(resp.content, 'lxml')
+ps = soup(resp.content)
 
 container = ps.findAll("span", {"id": "price_inside_buybox"})
 
