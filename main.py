40382519b8091b0e308e984b4ab0ca51bc373aae
Ryan Ereno <ryanaereno@gmail.com>
2020-11-09 11:06:22 -0800

    changed price to a global variable and added a function to get the price

--- main.py
+++ main.py
@@ -4,28 +4,25 @@ from bs4 import BeautifulSoup as soup
 
 EMAIL_ADDRESS = 'NotifyingPrice@gmail.com'
 EMAIL_PASSWORD = 'FuckChegg123'
-RECIPIENT = 'ryanaereno@gmail.com'
-MSG = 'The price for your item has reached your desired price! Purchase Now! \n'
-#hello
+RECIPIENT = ''
+MSG = 'The price for your item has lowered! Purchase Now! \n'
+HEADERS = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0",
+           "Accept-Encoding": "gzip, deflate",
+           "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT": "1",
+           "Connection": "close", "Upgrade-Insecure-Requests": "1"}
+price = None
+
+
 def main():
-    #telling amazon that im a real browser
-    headers = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:66.0) Gecko/20100101 Firefox/66.0", "Accept-Encoding":"gzip, deflate", "Accept":"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8", "DNT":"1","Connection":"close", "Upgrade-Insecure-Requests":"1"}
+    # this lets python know that price will be referring to the global one defined above
+    global price
 
-    my_url = 'http://rb.gy/kjkteb'
+    my_url = 'http://rb.gy/legkuo'
 
-    resp = requests.get(my_url, headers = headers)
+    price = getPrice(my_url)
 
-    ps = soup(resp.content, 'lxml')
+    checkPrice(price, my_url)
 
-    container = ps.findAll("span", {"id": "price_inside_buybox"})
-    temp_price = container[0].text
-
-    actual_price = float(temp_price[2:len(temp_price)])
-    print(actual_price)
-
-    my_price = float(input())
-
-    checkPrice(my_price, actual_price, my_url)
 
 def sendEmail(my_url):
     server = smtplib.SMTP_SSL('smtp.gmail.com', 465)
@@ -37,12 +34,31 @@ def sendEmail(my_url):
 
     server.quit()
 
-def checkPrice(my_price, actual_price, my_url):
-    if my_price <= actual_price:
-        sendEmail(my_url)
 
-    else:
-        print("Nah G")
+def checkPrice(original_price, my_url):
+    global price
+
+    new_price = getPrice(my_url)
+
+    if new_price < original_price:
+        sendEmail(my_url)
+        price = new_price
+
+    elif new_price > original_price:
+        price = new_price
+
+
+def getPrice(my_url):
+    resp = requests.get(my_url, headers=HEADERS)
+
+    ps = soup(resp.content, 'lxml')
+
+    container = ps.findAll("span", {"id": "price_inside_buybox"})
+    temp_price = container[0].text
+
+    new_price = float(temp_price[2:len(temp_price)])
+
+    return new_price
 
 
 main()
